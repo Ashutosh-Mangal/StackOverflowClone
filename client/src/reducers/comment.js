2f551bcb4483f0041d4797289b8c4e9a35389033
Ashutosh Mangal <ashutoshmangal99@gmail.com>
2023-01-15 16:28:28 +0530

    first commit

---
+++ client/src/reducers/comment.js
@@ -0,0 +1,13 @@
+const commentReducer = (state = { data: null }, action) => {
+  switch (action.type) {
+    case "POST_COMMENT":
+      return { ...state };
+    // case "POST_ANSWER":
+    //     return { ...state }
+    // case 'FETCH_ALL_QUESTIONS':
+    //     return { ...state, data: action.payload}
+    default:
+      return state;
+  }
+};
+export default commentReducer;
